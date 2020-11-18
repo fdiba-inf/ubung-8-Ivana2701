44fb5bb456a6f40f4a9efc41c2b6b8b5f1d8c2f2
Ivana2701 <ivanabaj@abv.bg>
2020-11-18 16:42:12 +0000

    ;

--- src/exercise8/Point.java
+++ src/exercise8/Point.java
@@ -61,7 +61,7 @@ public class Point {
     }
 
     public String toString() {
-        return "("+ x +", "+ y +")"
+        return "("+ x +", "+ y +")";
 
         // Represent attributes as string
        
